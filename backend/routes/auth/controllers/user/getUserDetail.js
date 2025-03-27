193d9e109cdb13c75bcbaf7108b522d1be1442c9
Your Name <lmcrean@gmail.com>
2025-03-27 07:03:56 +0000

    feat: add user management controllers for deletion, retrieval, and updating, enhancing backend user functionality and modularity

--- backend/routes/auth/controllers/user/getUserDetail.js
+++ backend/routes/auth/controllers/user/getUserDetail.js
@@ -1,4 +1,4 @@
-import User from '../../../models/User.js';
+import User from '../../../../models/User.js';
 
 /**
  * Get user by ID
---
+++ backend/routes/auth/controllers/user/index.js
@@ -0,0 +1,18 @@
+import { getAllUsers } from './getUserList.js';
+import { getUserById } from './getUserDetail.js';
+import { updateUser } from './updateUser.js';
+import { deleteUser } from './deleteUser.js';
+
+export {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+};
+
+export default {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+}; 
